722a3d261c568ecf2b86385daa1f757d7fdf282b
Riley Griffin <rigriffin241@gmail.com>
2018-11-24 11:58:23 -0500

    stuff

--- src/client/components/HomePage.jsx
+++ src/client/components/HomePage.jsx
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div id="hero" classs="wow fadeIn">
-                    <div className="hero-container">
-                        <h1>Riley Griffin</h1>
-                        <h2>Full-Stack Developer, MERN</h2>
-                        <img src="/imgs/me.PNG" />
-                    </div>
-                    <div className="links">
-                        
-                    </div>
-            </div>
+        <div>
+            
+        </div>
         )
     }
 }
